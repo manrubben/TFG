846522fe0fa66c7042471f2117027783dc9890ca
Manu <manrubben@alum.us.es>
2022-04-07 02:15:39 +0200

    Update auxiliar tests done

--- server/models/Users.js
+++ server/models/Users.js
@@ -3,14 +3,32 @@ module.exports = (sequelize, DataTypes) => {
         nombre: {
             type: DataTypes.STRING,
             allowNull: false,
+            validate: {
+                notEmpty: {
+                    args: true,
+                    msg: 'El nombre no puede estar vacío'
+                }
+            }
         },
         apellidos: {
             type: DataTypes.STRING,
             allowNull: false,
+            validate: {
+                notEmpty: {
+                    args: true,
+                    msg: 'Los apellidos no pueden estar vacíos'
+                }
+            }
         },
         telefono: {
             type: DataTypes.STRING,
             allowNull: false,
+            validate: {
+                notEmpty: {
+                    args: true,
+                    msg: 'El número de teléfono no puede estar vacío'
+                }
+            }
         },
         rol: {
             type: DataTypes.STRING,
@@ -19,10 +37,22 @@ module.exports = (sequelize, DataTypes) => {
         username: {
             type: DataTypes.STRING,
             allowNull: false,
+            validate: {
+                notEmpty: {
+                    args: true,
+                    msg: 'El nombre de usuario no puede estar vacío'
+                }
+            }
         },
         password: {
             type: DataTypes.STRING,
             allowNull: false,
+            validate: {
+                notEmpty: {
+                    args: true,
+                    msg: 'La contraseña no puede estar vacía'
+                }
+            }
         },
     });
 
